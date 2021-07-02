ff43242d72796ee68930ae827c4b72aba39ec187
boewoei0123 <b06902125@ntu.edu.tw>
2021-07-02 16:22:38 +0000

    final

--- Merge/run.sh
+++ Merge/run.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES='2'
+export CUDA_VISIBLE_DEVICES='1'
 
 #python ../CatChoice_WD/make_data.py -d ../datasets/data/dev -s ../datasets/data/schema.json -o cat_task_data/dev.jsonl
 #python ../CatChoice_WD/predict.py --test_file cat_task_data/dev.jsonl --target_dir ../CatChoice_WD/saved/large_align_val --out_file outputs/dev/cat_results.json
@@ -12,15 +12,15 @@ export CUDA_VISIBLE_DEVICES='2'
 #python eval.py -d ../datasets/data/dev -s ../datasets/data/schema.json  -i outputs/dev/results.json -c cat_bool
 #python eval.py -d ../datasets/data/dev -s ../datasets/data/schema.json  -i outputs/dev/results.json -c cat_text
 
-python ../CatChoice/make_data.py -d ../datasets/data/test_seen -s ../datasets/data/schema.json -o cat_task_data/test_seen.jsonl --norm
-python ../CatChoice/predict.py --test_file cat_task_data/test_seen.jsonl --target_dir ../CatChoice/saved/large_align_aug --out_file outputs/test_seen/cat_results.json
-python ../NoncatSpan/make_data.py -d ../datasets/data/test_seen -s ../datasets/data/schema.json -o noncat_task_data/test_seen.jsonl --norm
+python ../CatChoice_WD/make_data.py -d ~/data/test_seen -s ~/data/schema.json -o cat_task_data/test_seen.jsonl --norm
+python ../CatChoice_WD/predict.py --test_file cat_task_data/test_seen.jsonl --target_dir ../CatChoice_WD/saved/large_align_aug --out_file outputs/test_seen/cat_results.json
+python ../NoncatSpan/make_data.py -d ~/data/test_seen -s ~/data/schema.json -o noncat_task_data/test_seen.jsonl --norm
 python ../NoncatSpan/predict.py --test_file noncat_task_data/test_seen.jsonl --target_dir ../NoncatSpan/saved/large_align_aug --out_file outputs/test_seen/noncat_results.json
 python merge_to_csv.py --in_files outputs/test_seen/cat_results.json outputs/test_seen/noncat_results.json --out_csv outputs/test_seen/results.csv
 
-python ../CatChoice/make_data.py -d ../datasets/data/test_unseen -s ../datasets/data/schema.json -o cat_task_data/test_unseen.jsonl --norm
-python ../CatChoice/predict.py --test_file cat_task_data/test_unseen.jsonl --target_dir ../CatChoice/saved/large_align_aug --out_file outputs/test_unseen/cat_results.json
-python ../NoncatSpan/make_data.py -d ../datasets/data/test_unseen -s ../datasets/data/schema.json -o noncat_task_data/test_unseen.jsonl --norm
+python ../CatChoice_WD/make_data.py -d ~/data/test_unseen -s ~/data/schema.json -o cat_task_data/test_unseen.jsonl --norm
+python ../CatChoice_WD/predict.py --test_file cat_task_data/test_unseen.jsonl --target_dir ../CatChoice_WD/saved/large_align_aug --out_file outputs/test_unseen/cat_results.json
+python ../NoncatSpan/make_data.py -d ~/data/test_unseen -s ~/data/schema.json -o noncat_task_data/test_unseen.jsonl --norm
 python ../NoncatSpan/predict.py --test_file noncat_task_data/test_unseen.jsonl --target_dir ../NoncatSpan/saved/large_align_aug --out_file outputs/test_unseen/noncat_results.json
 python merge_to_csv.py --in_files outputs/test_unseen/cat_results.json outputs/test_unseen/noncat_results.json --out_csv outputs/test_unseen/results.csv
 
--- env/create.sh
+++ env/create.sh
@@ -1,4 +1,4 @@
 conda create -y -n dst python=3.8 && \
-conda install -y -n dst pytorch torchvision torchaudio cudatoolkit=11.1 -c pytorch -c nvidia && \
-conda install -y -n dst Cython && \
+conda install -y -n dst pytorch=1.8.1 torchvision torchaudio cudatoolkit=11.1 -c pytorch -c nvidia && \
+conda install -y -n dst Cython=0.29.23 && \
 conda run -n dst --no-capture-output pip install -r requirements.txt
