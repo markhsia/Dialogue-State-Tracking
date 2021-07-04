56daa92778f1fd21d765724ab3f68d0d6b5b8ccb
boewoei0123 <b06902125@ntu.edu.tw>
2021-07-04 11:23:46 +0000

    do some modification and deletion

--- CatChoice/run.sh
+++ CatChoice/run.sh
@@ -1,10 +1,10 @@
-export CUDA_VISIBLE_DEVICES='2'
+export CUDA_VISIBLE_DEVICES='0'
 #python make_data.py -d ../datasets/data/train -s ../datasets/data/schema.json -o task_data/train.jsonl -l -a 0.6 --norm
 #python make_data.py -d ../datasets/data/dev -s ../datasets/data/schema.json -o task_data/dev.jsonl -l --norm
 #python train.py --train_file task_data/train.jsonl --valid_file task_data/dev.jsonl --model_name roberta-large
 
-#python make_data.py -d ../datasets/data/train ../datasets/data/dev -s ../datasets/data/schema.json -o task_data/all.jsonl -l -a 0.6
-#python train.py --train_file task_data/all.jsonl --model_name roberta-large
+python make_data.py -d ../datasets/data/train ../datasets/data/dev -s ../datasets/data/schema.json -o task_data/all.jsonl -l -a 0.6 --norm
+python train.py --train_file task_data/all.jsonl --model_name roberta-large
 
-python make_data.py -d ../datasets/data/dev -s ../datasets/data/schema.json -o task_data/dev.jsonl -l --norm
-python eval.py --valid_file task_data/dev.jsonl --target_dir saved/large_align_aug_val
+#python make_data.py -d ../datasets/data/dev -s ../datasets/data/schema.json -o task_data/dev.jsonl -l --norm
+#python eval.py --valid_file task_data/dev.jsonl --target_dir saved/large_align_aug_val
--- NoncatSpan/run.sh
+++ NoncatSpan/run.sh
@@ -3,9 +3,9 @@ export CUDA_VISIBLE_DEVICES='0'
 #python make_data.py -d ../datasets/data/dev -s ../datasets/data/schema.json -o task_data/dev.jsonl -l --keep_no_matched --norm
 #python train.py --train_file task_data/train.jsonl --valid_file task_data/dev.jsonl --model_name xlnet-large-cased --epoch 5
 
-#python make_data.py -d ../datasets/data/train ../datasets/data/dev -s ../datasets/data/schema.json -o task_data/all.jsonl -l -a 0.6
-#python train.py --train_file task_data/all.jsonl --model_name xlnet-large-cased
+python make_data.py -d ../datasets/data/train ../datasets/data/dev -s ../datasets/data/schema.json -o task_data/all.jsonl -l -a 0.6 --norm
+python train.py --train_file task_data/all.jsonl --model_name xlnet-large-cased
 
 
-python make_data.py -d ../datasets/data/dev -s ../datasets/data/schema.json -o task_data/dev.jsonl -l --keep_no_matched --norm
-python eval.py --valid_file task_data/dev.jsonl --target_dir saved/0629-1704
+#python make_data.py -d ../datasets/data/dev -s ../datasets/data/schema.json -o task_data/dev.jsonl -l --keep_no_matched --norm
+#python eval.py --valid_file task_data/dev.jsonl --target_dir saved/0629-1704
